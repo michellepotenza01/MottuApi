0229f3905b18968e1c46b556f5dc380d439055f6
michellepotenza01 <michellepotenza73533@gmail.com>
2025-05-22 02:12:55 -0300

    Mais ajustes

--- Program.cs
+++ Program.cs
@@ -4,14 +4,6 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-<<<<<<< HEAD
-builder.Services.AddDbContext<MottuDbContext>(options =>
-    options.UseOracle(builder.Configuration.GetConnectionString("OracleConnection")));
-
-builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-=======
 // Configuração do DbContext
 builder.Services.AddDbContext<MottuDbContext>(options =>
     options.UseOracle(builder.Configuration.GetConnectionString("OracleConnection")));
@@ -25,7 +17,6 @@ builder.Services.AddControllers()
     });
 
 // Configuração do Swagger para documentação da API
->>>>>>> 62f9b743ffb9732fdc1a0ab84ee219aab1dbd018
 builder.Services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new OpenApiInfo
@@ -35,34 +26,25 @@ builder.Services.AddSwaggerGen(options =>
         Description = "API para gestão de motos, funcionários, pátios e clientes."
     });
 
-<<<<<<< HEAD
-    options.TagActionsBy(api => [api.ActionDescriptor.RouteValues["controller"]]);
-=======
-// Aqui configuramos o Kestrel diretamente para escutar na porta 80
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.ListenAnyIP(80);  // Configura Kestrel para escutar na porta 80 em todas as interfaces
->>>>>>> 62f9b743ffb9732fdc1a0ab84ee219aab1dbd018
+    options.TagActionsBy(api => new[] { api.ActionDescriptor.RouteValues["controller"] });
 });
+
+
 
 var app = builder.Build();
 
-<<<<<<< HEAD
-=======
-// Configuração do Swagger UI para visualização e testes
->>>>>>> 62f9b743ffb9732fdc1a0ab84ee219aab1dbd018
+// Configuração do Swagger UI
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mottu API v1");
-        c.RoutePrefix = string.Empty;  // Acesso ao Swagger diretamente na raiz
+        c.RoutePrefix = string.Empty; // Swagger acessível em "/"
     });
 }
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
-
 app.Run();
